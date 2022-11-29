18c05bace842def341f75a6c3c89ba77eb291eb4
Al Corbett <alan.corbett@cvo.com>
2022-11-29 12:18:13 -0800

    initial

---
+++ geofencing/Sources/Geofencing/Geofencing.swift
@@ -0,0 +1,7 @@
+public struct Geofencing {
+    public private(set) var text = "com.prepass.mobile.ios.geofencing"
+
+    public init() {
+    }
+
+}
---
+++ geofencing/Sources/Geofencing/GeofencingManager.swift
@@ -0,0 +1,17 @@
+	//
+	//  GeoFencingManager.swift
+	//  GeioFencingPackage
+	//
+	//  Created by Alan.Corbett on 11/2/22.
+	//
+
+import Foundation
+import SwiftUI
+import MapKit
+
+enum FileLoadError: Error {
+	case fileNotFound(String)
+}
+
+struct GeoFencingManager {
+}
